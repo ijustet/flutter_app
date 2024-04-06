0a4924dc6c7c9fc315d6d3a02d845992e97e5587
kamemura <ttsjkmmr.1ii0ii@gmail.com>
2024-04-06 23:49:30 +0900

    4-2まで対応

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.pink,
         primaryColor: const Color(0xFFe91e63),
-        accentColor: const Color(0xFFe91e63),
         canvasColor: const Color(0xFFfafafa),
       ),
       home: new MyHomePage(),
@@ -23,103 +22,56 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static var _message = 'ok.';
-  static var _stars = '☆☆☆☆☆';
-  static var _star = 0;
+  static var _message = 'ok';
   static var _index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My app'),
-        leading: BackButton(
-          color: Colors.white,
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.android), 
-            tooltip: 'add star...',
-            onPressed: iconPressedA,
-          ),
-          IconButton(
-            icon: Icon(Icons.favorite), 
-            tooltip: 'subtract star...',
-            onPressed: iconPressedB,
-          ),
-        ],
-        bottom: PreferredSize(
-          child: Center(
-            child:Text(_stars,
-              style: TextStyle(
-                fontSize: 22.0,
-                color: Colors.white
-              ),
-            )
-          ), 
-          preferredSize: const Size.fromHeight(30.0)
-        ),
+        title: Text('My App'),
       ),
-      
       body: Center(
         child: Text(
           _message,
           style: const TextStyle(
-            fontSize: 28.0
+            fontSize: 28.0,
           ),
         )
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _index,
         backgroundColor: Colors.lightBlueAccent,
-        items:  <BottomNavigationBarItem>[
+        items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            title: Text('Android'),
-            icon: const Icon(Icons.android,color: Colors.black,size: 50),
+            label: 'Android',
+            icon: Icon(Icons.android,color: Colors.black, size: 50),
           ),
           BottomNavigationBarItem(
-            title: Text('Favorite'),
-            icon: const Icon(Icons.favorite,color: Colors.red,size: 50),
+            label: 'Favorite',
+            icon: Icon(Icons.favorite,color: Colors.red, size: 50),
           ),
           BottomNavigationBarItem(
-            title: Text('Home'),
-            icon: const Icon(Icons.home,color: Colors.white,size: 50),
+            label: 'Home',
+            icon: Icon(Icons.home,color: Colors.white, size: 50),
           ),
         ],
         onTap: tapBottomIcon,
-  
       ),
     );
   }
-  void tapBottomIcon(int value){
-    var items = ['android','Heart','Home'];
+
+  void tapBottomIcon(int value) {
+    var items = ['Android', 'Heart', 'Home'];
     setState(() {
       _index = value;
-      _message = 'you tapped: "'+ items[_index] +'".';
-    });
-  }
-  void iconPressedA(){
-    _message = 'tap "android".';
-    _star++;
-    update();
-  }
-  void iconPressedB(){
-    _message = 'tap "favorite".';
-    _star--;
-    update();
-  }
-  void update(){
-    _star = _star < 0 ? 0 : _star > 5 ? 5 : _star;
-    setState(() {
-      _stars = '★★★★★☆☆☆☆☆'.substring(5 - _star, 5 - _star + 5);
-      _message = _message + '[$_star]';
+      _message = 'you tapped: "' + items[_index] + '".';
     });
   }
 }
-

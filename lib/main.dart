c939b639fdbaa0d1e4e013bb4c288464ca5c3b92
kamemura <ttsjkmmr.1ii0ii@gmail.com>
2024-09-23 18:00:48 +0900

    4-12 Drawer実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,81 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Generated App',
-      theme: new ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
         primaryColor: const Color(0xff2196f3),
-        canvasColor: const Color(0xFFfafafa),
+        canvasColor: const Color(0xfffafafa),
       ),
-      initialRoute: '/',
-      routes: {
-        '/' : (context) => FirstScreen(),
-        '/second' : (context) => SecondScreen('Second'),
-        '/third' : (context) => SecondScreen('Third'),
-      },
+      home: MyHomePage(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static var _items = <Widget>[];
+  static var _message = 'ok.';
+  static var _tapped = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    for (var i = 0; i < 5; i++) {
+      var item = ListTile(
+          leading: const Icon(Icons.android),
+          title: Text('No, $i'),
+          onTap: (){
+            _tapped = i;
+            tapItem();
+          }
+      );
+      _items.add(item);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body:Column(
-        children: <Widget>[
-          const Text(
-            'Home Screen',
-            style: const TextStyle(fontSize: 32),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
-        items: <BottomNavigationBarItem>[
-          const BottomNavigationBarItem(
-            label: 'Home',
-            icon: const Icon(Icons.home, size: 32,),
-          ),
-          const BottomNavigationBarItem(
-            label: 'next',
-            icon: const Icon(Icons.navigate_next, size: 32,),
-          )
-        ],
-        onTap: (int value){
-          if(value == 1)
-            Navigator.pushNamed(context, '/second');
-        },
-      ),
-    );
-  }
-}
-class SecondScreen extends StatelessWidget {
-  final String _value;
-  SecondScreen(this._value);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Next'),
+        title: const Text('Flutter App'),
       ),
       body: Center(
         child: Text(
-          '$_value Screen', 
-          style: const TextStyle(fontSize: 32.0),
+          _message,
+          style: const TextStyle(
+            fontSize: 32.0,
+          ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: <BottomNavigationBarItem>[
-          const BottomNavigationBarItem(
-            label: 'prev',
-            icon: const Icon(Icons.navigate_before, size: 32,),
-          ),
-          const BottomNavigationBarItem(
-            label: '?',
-            icon: const Icon(Icons.android, size: 32,),
-          )
-        ],
-        onTap: (int value){
-          if(value == 0) Navigator.pop(context);
-          if(value == 1) Navigator.pushNamed(context, '/third');
-        },
+      drawer: Drawer(
+        child: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(20.0),
+          children: _items,
+        ),
       ),
     );
+  }
+
+  void tapItem() {
+    Navigator.pop(context);
+    setState((){
+      _message = 'tapped:[$_tapped]';
+    });
   }
 }
